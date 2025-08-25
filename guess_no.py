22b33d8471038fc798b5841d5e790a31fe8a5299
Tapasbiswal2000 <tapas637076@gmail.com>
2025-08-25 19:46:08 +0530

    this is the game

---
+++ guess_no.py
@@ -0,0 +1,23 @@
+import random
+
+random_no = random.randint(1,1000)
+
+while True:
+    try:        
+        userChoice = input("Guess a number and for quit press Q  : ")
+        if(userChoice == "Q"):
+            break
+        userChoice = int(userChoice)
+        if(userChoice == random_no):
+            print("YOU SUCCESSfully did it")
+            break
+
+        elif(userChoice > random_no):
+            print("You are going too big , Guess smaller.")
+
+        else:
+            print("You are going too small , Guess bigger.")
+    except ValueError:
+        print("invalid choice.")
+
+print("----GAME OVER----")
